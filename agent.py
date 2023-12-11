d486bb481c27ff4adf0baaa0345bc6f9cd8093b5
Mustafa <musto@Mustafas-MacBook-Air.local>
2023-12-10 18:04:13 -0800

    Removed Bug in Naive Agent, decoupled python files.

---
+++ agent.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+class Agent:
+    def __init__(self, loaded_policy=None, action_type="continous"):
+        self.loaded_policy = loaded_policy
+        self.action_type = action_type
+
+    def select_action(self):
+        raise NotImplementedError("This method should be implemented by subclasses.")
+
+class RandomAgent(Agent):
+    def __init__(self, action_space, action_type="continous", momentum_length=None):
+        super().__init__(action_type=action_type)
+        self.action_space = action_space # boundaries of the action space if continous; num of actions in discrete
+
+        self.momentum_length = momentum_length
+        self.actions_in_a_row = 0
+        self.last_action = None
+
+    def select_action(self, observation):
+        if self.action_type == "continous":
+            x_action = np.random.uniform(self.action_space[0][0], self.action_space[0][1])
+            y_action = np.random.uniform(self.action_space[1][0], self.action_space[1][1])
+            return (x_action, y_action)
+        else:
+            if self.actions_in_a_row < self.momentum_length and self.last_action is not None:
+                self.actions_in_a_row += 1
+                return self.last_action
+            else:
+                self.actions_in_a_row = 0
+                self.last_action = np.random.choice(self.action_space)
+                return self.last_action
+            
+        
+    
+class UniformAgent(Agent):
+    def __init__(self, uniform_action,action_type="continous"):
+        super().__init__(action_type)
+        self.uniform_action = uniform_action # number in discrete; tuple is continous
+
+    def select_action(self, observation):
+        return self.uniform_action
+    
+
+class NaiveAgent(Agent):
+    def __init__(self, target, num_actions, action_type="discrete", magnitude=1):
+        super().__init__(action_type=action_type)
+        self.target = target
+        self.num_actions = num_actions
+        self.magnitude = magnitude
+        self.action_map = {i: (np.cos(2 * np.pi * i / num_actions), np.sin(2 * np.pi * i / num_actions)) for i in range(num_actions)}
+
+    def select_action(self, observation):
+        # Calculate the angle between the current position and the target
+        dx = self.target[0] - observation[0]
+        dy = self.target[1] - observation[1]
+        angle = np.arctan2(dy, dx)
+
+        if self.action_type == "continous":
+            return (self.magnitude * np.cos(angle), self.magnitude * np.sin(angle))
+        else:
+            # Calculate the index of the action that is closest to the angle
+            action = int(np.round(angle * self.num_actions / (2 * np.pi))) % self.num_actions
+            #print("----------------")
+            #print("Action: ", action)
+            #print("Angle: ", (180 / np.pi) * angle)
+            #print("----------------")
+
+        return action
---
+++ create_offline_data.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import random
+import time 
+import pickle
+from agent import NaiveAgent
+
+from simulator import Environment, SingleGyreFlowField
+
+
+NUM_ACTIONS = 8
+MAGNITUDE = 1 # Magnitude of the action vector
+RENDER = True
+
+
+
+class ReplayBuffer:
+    def __init__(self):
+        self.buffer = {"state":[], "action":[], "next_state":[], "reward":[], "done":[]}
+
+    def add(self, state, action, next_state, reward, done):
+        self.buffer["state"].append(state)
+        self.buffer["action"].append(action)
+        self.buffer["next_state"].append(next_state)
+        self.buffer["reward"].append(reward)
+        self.buffer["done"].append(done)
+
+    def get_trajectory(self):
+        return self.buffer
+
+    def clear(self):
+        self.buffer = []
+
+
+def create_random_coordinate(x_bounds, y_bounds):
+    x = random.uniform(*x_bounds)
+    y = random.uniform(*y_bounds)
+    return x, y
+
+def generate_random_trajectories(start_sample_area_interval, target_sample_area_interval, flow_field, num_rollouts, max_steps):
+    ''' Fill up the replay buffer with random trajectories. '''
+    buffer = ReplayBuffer()
+    for i in range(num_rollouts):
+        print("Rollout:", i)
+        start = create_random_coordinate(*start_sample_area_interval)
+        target = create_random_coordinate(*target_sample_area_interval)
+        print("Start:", start)
+        print("Target:", target)
+        # agent = RandomAgent(momentum_length=3, action_space=NUM_ACTIONS, action_type="discrete")#action_space=((0, 1), (0, 1)))
+        agent = NaiveAgent(target, NUM_ACTIONS)
+        env = Environment(flow_field, list(start), target, threshold=1.0,
+                           buffer=buffer, action_type="discrete", num_actions=NUM_ACTIONS, magnitude=MAGNITUDE)
+        state = env.reset()
+        done = False
+        step = 0
+        while not done and step < max_steps:
+            action = agent.select_action(state)
+            new_state, action, reward, done = env.step(action)
+            #print(action) - works
+            #print(new_state[0]) - works
+            step += 1
+        print("Last coordinate:", env.current_state[0])
+        if RENDER:
+            env.render()
+    return buffer
+
+
+
+
+if __name__ == "__main__":
+    # Usage of random trajectories
+    # action_space = ((0, 1), (0, 1)) 
+    flow_field = SingleGyreFlowField(width=20, height=20, center=(10, 10), radius=4, strength=1)
+    buffer = generate_random_trajectories(
+        start_sample_area_interval=[(1,3),(1,3)],
+        target_sample_area_interval=[(16, 19), (16, 19)],
+        flow_field=flow_field,
+        num_rollouts=100,
+        max_steps=100)
+    print("Number of trajectories:", len(buffer.buffer))
+    # Pickl the buffer and dump it to a file 
+    current_time = time.strftime("%Y%m%d-%H%M%S")
+    filename = f"logs/buffer_{current_time}.pkl"
+    with open(filename, "wb") as f:
+        pickle.dump(buffer.buffer, f)
+        print("Buffer saved to", filename)
